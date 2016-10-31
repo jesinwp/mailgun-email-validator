b7fd222596bf3489b6828b5fbf9c54f927537bb4
Jesin A <me@jesin.in>
2016-11-01 02:21:35 +0530

    WordPress Coding standards
    Part 1 of bring this file to the WordPress PHP and JS coding standards.

--- admin_options.php
+++ admin_options.php
@@ -1,12 +1,9 @@
 <?php
-if( !class_exists( 'Email_Validation_Mailgun_Admin' ) )
-{
-	class Email_Validation_Mailgun_Admin
-	{
+if ( ! class_exists( 'Email_Validation_Mailgun_Admin' ) ) {
+	class Email_Validation_Mailgun_Admin {
 		private $options = NULL;
 
-		public function __construct()
-		{
+		public function __construct() {
 			$this->options = get_option( 'jesin_mailgun_email_validator' );
 
 			add_action( 'admin_menu' , array( &$this, 'plugin_menu' ) );
@@ -15,16 +12,14 @@ if( !class_exists( 'Email_Validation_Mailgun_Admin' ) )
 		}
 
 		//Display admin notices
-		public function admin_messages()
-		{
+		public function admin_messages() {
 			global $email_validation_mailgun;
 			//Displayed if no API key is entered
 			if( !isset( $this->options['mailgun_pubkey_api'] ) || empty( $this->options['mailgun_pubkey_api'] ) )
 				echo '<div class="updated"><p>' . sprintf( __( 'The %s will not work until a %s is entered.', $email_validation_mailgun->slug ), '<a href="'.admin_url( 'options-general.php?page=' . $email_validation_mailgun->slug ).'">Mailgun Email Validator plugin</a>', 'Mailgun Public API key' ) . '</p></div>';
 		}
 		
-		public function settings_link( $links )
-		{
+		public function settings_link( $links ) {
 			global $email_validation_mailgun;
 			array_unshift( $links, '<a href="'.admin_url( 'options-general.php?page=' . $email_validation_mailgun->slug ).'">' . __( 'Settings', $email_validation_mailgun->slug ) . '</a>' );
 			$links[] = '<a href="http://websistent.com/wordpress-plugins/" target="_blank" title="' . sprintf( __( 'More Plugins by %s', $email_validation_mailgun->slug ), 'Jesin' ) . '">' . __( 'More Plugins', $email_validation_mailgun->slug ) . '</a>';
@@ -32,8 +27,7 @@ if( !class_exists( 'Email_Validation_Mailgun_Admin' ) )
 		}
 
 		//Hook in and create a menu
-		public function plugin_menu()
-		{
+		public function plugin_menu() {
 			global $email_validation_mailgun;
 			add_filter( 'plugin_action_links_' . $email_validation_mailgun->basename, array( &$this, 'settings_link' ) );
 			$plugin_page = add_options_page( __( 'Email Validation Settings', $email_validation_mailgun->slug ), __( 'Email Validation', $email_validation_mailgun->slug ), 'manage_options', $email_validation_mailgun->slug, array( &$this, 'plugin_options' ) );
@@ -42,8 +36,7 @@ if( !class_exists( 'Email_Validation_Mailgun_Admin' ) )
 		}
 
 		//Create the options page
-		public function plugin_settings()
-		{
+		public function plugin_settings() {
 			add_action( 'wp_ajax_mailgun_api', array( &$this, 'mailgun_api_ajax_callback') ); //AJAX to verify the API key
 			add_action( 'wp_ajax_test_email', array( &$this, 'test_email_ajax_callback') ); //AJAX for demo email validation
 
@@ -53,72 +46,65 @@ if( !class_exists( 'Email_Validation_Mailgun_Admin' ) )
 			add_settings_field('mailgun_pubkey_api','Mailgun Public API', array( &$this, 'api_field' ), $email_validation_mailgun->slug, $email_validation_mailgun->slug.'_settings', array( 'label_for' => 'mailgun_pubkey_api' ) ); //Public API key field
 		}
 
-		public function plugin_panel_styles()
-		{
+		public function plugin_panel_styles() {
 			global $email_validation_mailgun;
 			echo '<style type="text/css">#icon-'.$email_validation_mailgun->slug.'{background:transparent url(\'' . plugin_dir_url( __FILE__ ) . 'screen-icon.png\') no-repeat;}</style>';
 		}
 
 		//Add AJAX to the footer
-		public function plugin_panel_scripts()
-		{
+		public function plugin_panel_scripts() {
 			global $email_validation_mailgun;
 ?>
 <script type="text/javascript">
-jQuery(document).ready(
-	jQuery('#mailgun_api_verify').click (function($) 
-	{
-		if (jQuery.trim(jQuery('#mailgun_pubkey_api').val()).length == 0) {
-			jQuery('#api_output').html('<?php _e( 'This field cannot be empty', $email_validation_mailgun->slug ); ?>');
+jQuery( document ).ready(
+	jQuery( '#mailgun_api_verify' ).click ( function( $ ) {
+		if ( jQuery.trim( jQuery( '#mailgun_pubkey_api' ).val() ).length == 0 ) {
+			jQuery( '#api_output' ).html( '<?php _e( 'This field cannot be empty', $email_validation_mailgun->slug ); ?>' );
 			return;
 		}
 
 		var data = {
 			action: 'mailgun_api',
-			api: jQuery('#mailgun_pubkey_api').val()
+			api: jQuery( '#mailgun_pubkey_api' ).val()
 		};
 
-		jQuery('#api_output').html('<?php _e( 'Checking', $email_validation_mailgun->slug ); ?>...');
-		jQuery('#api_output').css("cursor","wait");
-		jQuery('#mailgun_api_verify').attr("disabled","disabled");
-		jQuery.post(ajaxurl, data, function(response) {
-			jQuery('#api_output').html(response);
-			jQuery('#api_output').css("cursor","default");
-			jQuery('#mailgun_api_verify').removeAttr("disabled");
-		}
-		);
+		jQuery( '#api_output' ).html( '<?php _e( 'Checking', $email_validation_mailgun->slug ); ?>...' );
+		jQuery( '#api_output' ).css( "cursor", "wait" );
+		jQuery( '#mailgun_api_verify' ).attr( "disabled", "disabled" );
+		jQuery.post( ajaxurl, data, function( response ) {
+			jQuery( '#api_output' ).html( response );
+			jQuery( '#api_output' ).css( "cursor", "default" );
+			jQuery( '#mailgun_api_verify' ).removeAttr( "disabled" );
+		});
 	}
 ));
 
-jQuery(document).ready(
-	jQuery('#validate_email').click (function($)
-	{
-		if (jQuery.trim(jQuery('#sample_email').val()).length == 0) {
-			jQuery('#email_output').html('<?php _e( 'Please enter an email address to validate', $email_validation_mailgun->slug ); ?>');
+jQuery( document ).ready(
+	jQuery( '#validate_email' ).click ( function( $ ) {
+		if ( jQuery.trim( jQuery( '#sample_email' ).val() ).length == 0 ) {
+			jQuery( '#email_output' ).html('<?php _e( 'Please enter an email address to validate', $email_validation_mailgun->slug ); ?>');
 			return;
 		}
 
 		var data = {
 			action: 'test_email',
-			email_id: jQuery('#sample_email').val()
+			email_id: jQuery( '#sample_email' ).val()
 		};
-		jQuery('#email_output').html('<?php _e( 'Checking', $email_validation_mailgun->slug ); ?>...');
-		jQuery('#email_output').css("cursor","wait");
-		jQuery('#validate_email').attr("disabled","disabled");
-		jQuery.post(ajaxurl, data, function(response) {
-			jQuery('#email_output').html(response);
-			jQuery('#email_output').css("cursor","default");
-			jQuery('#validate_email').removeAttr("disabled");
-		}
-		);
+		jQuery( '#email_output' ).html('<?php _e( 'Checking', $email_validation_mailgun->slug ); ?>...');
+		jQuery( '#email_output' ).css( "cursor", "wait");
+		jQuery( '#validate_email' ).attr( "disabled", "disabled");
+		jQuery.post( ajaxurl, data, function(response) {
+			jQuery( '#email_output' ).html( response );
+			jQuery( '#email_output' ).css( "cursor", "default" );
+			jQuery( '#validate_email' ).removeAttr( "disabled" );
+		});
 	}
 ));
 </script>
 <?php	}
 
 		//AJAX Callback function for validating the Public API key
-		public function mailgun_api_ajax_callback()
-		{
+		public function mailgun_api_ajax_callback() {
 			global $email_validation_mailgun;
 
 			$args = array(
@@ -132,27 +118,24 @@ jQuery(document).ready(
 			$response = wp_remote_request( "https://api.mailgun.net/v3/address/validate?address=foo%40mailgun.net", $args );
 
 			//A Network error has occurred
-			if( is_wp_error($response) )
+			if ( is_wp_error($response) ) {
 				echo '<span style="color:red">' . $response->get_error_message() . '</span>';
-			
-			elseif( isset($response->errors['http_request_failed']) )
-			{
+			} elseif ( isset( $response->errors['http_request_failed'] ) ) {
 				echo '<span style="color:red">' . __( 'The following error occurred when validating the key.', $email_validation_mailgun->slug ) . '<br />';
-				foreach($response->errors['http_request_failed'] as $http_errors)
+				foreach ( $response->errors['http_request_failed'] as $http_errors ) {
 					echo $http_errors;
+				}
 				echo '</span>';
-			}
-
-			elseif( '200' == $response['response']['code'] )
+			} elseif ( '200' == $response['response']['code'] ) {
 				echo '<span style="color:green">' . __( 'API Key is valid', $email_validation_mailgun->slug ) . '</span>';
-
-			//Invalid API as Mailgun returned 401 Unauthorized
-			elseif( '401' == $response['response']['code'] )
+			} //Invalid API as Mailgun returned 401 Unauthorized
+			elseif ( '401' == $response['response']['code'] ) {
 				echo '<span style="color:red">' . sprintf( __( 'Invalid API Key. Error code: %s %s', $email_validation_mailgun->slug ), $response['response']['code'], $response['response']['message'] ) . '</span>';
-
+			}
 			//A HTTP error other than 401 has occurred
-			else
+			else {
 				echo '<span style="color:red">' . sprintf( __( 'A HTTP error occurred when validating the API. Error code: %s %s', $email_validation_mailgun->slug ), $response['response']['code'], $response['response']['message'] ) . '</span>';
+			}
 
 			die();
 		}
